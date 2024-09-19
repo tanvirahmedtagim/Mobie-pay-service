c5db020d815fcadf085ec89bdff1d421ea195320
tanvirahmedtagim <tanvirahmedtagim@gmail.com>
2024-09-19 13:04:37 +0600

    home page layout added

--- js/login.js
+++ js/login.js
@@ -9,5 +9,13 @@ document
     console.log("login button clicked");
     //step-3: get the phone number
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+    // console.log(phoneNumber, pinNumber);
+    //bad way to validate
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      console.log("you are logged in");
+      window.location.href = './home.html';
+    } else {
+      alert("Wrong pin or password");
+    }
   });
